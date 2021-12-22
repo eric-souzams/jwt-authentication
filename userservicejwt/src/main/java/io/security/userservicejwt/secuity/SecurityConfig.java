fc01befb516a5e182d5eef498db3140355320ace
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2021-12-22 12:05:43 -0300

    wip: Created and implemented a authentication filter

--- userservicejwt/src/main/java/io/security/userservicejwt/secuity/SecurityConfig.java
+++ userservicejwt/src/main/java/io/security/userservicejwt/secuity/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.security.userservicejwt.secuity;
 
+import io.security.userservicejwt.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @RequiredArgsConstructor
 @Configuration @EnableWebSecurity
@@ -23,6 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
     }
 }
